f43d1071c32ac46c5c16ef0cd8f2a5b65a3cf686
Kumaran-23 <kumz.nair@gmail.com>
2023-06-29 16:47:09 +0800

    slight button color change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,17 +8,17 @@
 <div class="bg-gradient-to-r from-purple-500 via-fuchsia-500 to-pink-500">
   <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-10 mx-auto pt-20 pb-10" style="width: 1200px;">
   {#each data.images as image}
-    <div class="card hover:transition delay-150 hover:-translate-y-10 shadow-xl shadow-sky-400 hover:shadow-indigo-600 flex flex-col justify-between">
+    <div class="card hover:transition delay-150 hover:-translate-y-10 shadow-xl shadow-sky-200 hover:shadow-indigo-600 flex flex-col justify-between">
       <figure style="height: 300px;" class="relative">
         <img src={image.url} alt="" class="w-full h-full object-cover transition-transform duration-300 transform hover:scale-110" />
       </figure>
-      <div class="card-body flex-grow bg-gradient-to-r from-green-500  to-teal-500 rounded-b-2xl">
+      <div class="card-body flex-grow bg-gradient-to-r from-green-400 to-teal-400 rounded-b-2xl">
         <h2 class="card-title text-lg font-bold mb-2">{image.title}</h2>
         <p class="mb-4">{image.discription}</p>
         <p class="">${image.price/100} USD</p>
         <div class="card-actions justify-end mt-4">
           <a href="/images/{image.id}">
-            <button class="btn btn-primary hover:animate-pulse hover:btn-accent">View</button>
+            <button class="btn btn-primary hover:animate-pulse hover:btn-secondary">View</button>
           </a>
         </div>
       </div>
